f5a44a511da3ba3f277d18f2f2f08c19b6e03a06
Елена Рубцова <elena.rub2000@gmail.com>
2023-06-03 14:43:21 +0300

    Запрос по api с помощью curl к сервису openweathermap.org

---
+++ api/index.php
@@ -0,0 +1,57 @@
+<?php
+	// работа с API
+	// (https://openweathermap.org/)
+	// https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=b36dabcfdca44083202c1d7b0862a8f6
+
+	$url = 'https://api.openweathermap.org/data/2.5/weather';
+	$options = array(
+		'lat' => '44.34',
+		'lon' => '10.99',
+		'appid' => 'b36dabcfdca44083202c1d7b0862a8f6'
+	);
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL, $url.'?'.http_build_query($options));
+
+	$response = curl_exec($ch);
+	$data = json_decode($response, true);
+	curl_close($ch);
+
+	echo '<pre>';print_r($data);
+	echo '</pre>';
+
+// Вывод на экран в браузере:
+/* Array
+(
+    [coord] => Array
+        (
+            [lon] => 10.99
+            [lat] => 44.34
+        )
+
+    [weather] => Array
+        (
+            [0] => Array
+                (
+                    [id] => 802
+                    [main] => Clouds
+                    [description] => scattered clouds
+                    [icon] => 03d
+                )
+
+        )
+
+    [base] => stations
+    [main] => Array
+        (
+            [temp] => 294.44
+            [feels_like] => 294.43
+            [temp_min] => 292.56
+            [temp_max] => 295.42
+            [pressure] => 1015
+            [humidity] => 69
+            [sea_level] => 1015
+            [grnd_level] => 931
+        )
+... */
